163b398dcd2ba272fa10d3e35fb53403af2b3e33
Jiwhan <>
2022-06-30 22:58:56 +0900

    잠깐 백업

--- LTP/Engine/private/PhysXMgr.cpp
+++ LTP/Engine/private/PhysXMgr.cpp
@@ -1,6 +1,8 @@
 #include "..\Public\PhysXMgr.h"
 #include "ExternFont.h"
 
+#define PVD_HOST "127.0.0.1"
+
 IMPLEMENT_SINGLETON(CPhysXMgr)
 
 CPhysXMgr::CPhysXMgr()
@@ -21,25 +23,47 @@ HRESULT CPhysXMgr::Initialize_PhysX(ID3D11Device * pDevice, ID3D11DeviceContext 
 	return S_OK;
 }
 
+static PxDefaultErrorCallback gDefaultErrorCallback;
+static PxDefaultAllocator gDefaultAllocatorCallback;
+PxFoundation* gFoundation;
+
 void CPhysXMgr::TestPhysX()
 {
 	// 초기화용 변수
-	PxDefaultAllocator			mAllocCallback;
-	PxDefaultErrorCallback		mErrorCallback;
-	PxDefaultCpuDispatcher*		mDispatcher;
-	PxTolerancesScale			mTolerancesScale;
-	PxFoundation*				mFoundation;
-	PxPhysics*					mPhyscis;
-	PxScene*					mScene;
-	PxMaterial*					mMaterial;
-	PxPvd*						mPvd;
+//	PxDefaultAllocator			mAllocCallback;
+//	PxDefaultErrorCallback		mErrorCallback;
 	
+	PxFoundation*				mFoundation;
+	PxPvd*						mPvd;
+	PxPhysics*					mPhysics;
 	// Init
-	mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, mAllocCallback, mErrorCallback);
+
+
+
+	mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback,
+		gDefaultErrorCallback);
 	NULL_CHECK_BREAK(mFoundation);
 
-//	mPvd = PxCreatePvd(*mFoundation);
 
+	//bool recordMemoryAllocations = true;
+
+	//mPvd = PxCreatePvd(*gFoundation);
+	//PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
+	//mPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+
+	//mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation,
+	//	PxTolerancesScale(), recordMemoryAllocations, mPvd);
+	//NULL_CHECK_BREAK(mPhysics);
+
+
+
+
+//	PxSceneDesc desc(mPhyscis->getTolerancesScale());
+//	desc.gravity = PxVec3(0, -9.8f, 0.0f);
+//	mDispatcher = PxDefaultCpuDispatcherCreate(2);
+//	desc.cpuDispatcher = mDispatcher;
+//	desc.filterShader = PxDefaultSimulationFilterShader;
 
 }
 
